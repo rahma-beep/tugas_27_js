2f902dd6f648264153054fde2cf5e99890f3e5ec
Nur Rahmadani <nagamas.rahma@gmail.com>
2021-06-17 23:33:52 +0800

    TUGAS 27 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas27.js
@@ -0,0 +1,13 @@
+var a = "No";
+var b = " rumah saya ";
+var c = 30;
+var d = "A";
+
+var result1= a+b+c+d;
+console.log(result1);
+
+var x = "Jalan pangeran Tirtayasa No ";
+var y = 70;
+
+var result2 = x+y;
+console.log(result2);
